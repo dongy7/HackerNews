95894ea496c16b86aca13bb3407c00c325e4d641
dongy7 <dongy7@gmail.com>
2017-01-05 16:48:17 -0500

    add news list component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import * as injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { orangeA700 } from 'material-ui/styles/colors';
 import Navbar from './components/Navbar';
+import NewsList from './components/NewsList';
 
 injectTapEventPlugin();
 
@@ -19,6 +20,7 @@ class App extends React.Component<null, null> {
       <MuiThemeProvider muiTheme={muiTheme}>
         <div className="App">
           <Navbar />
+          <NewsList />
         </div>
       </MuiThemeProvider>
     );
