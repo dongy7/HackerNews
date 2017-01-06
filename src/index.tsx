d205e66046972f827e5dd04f8e6f406a7d4747a7
dongy7 <dongy7@gmail.com>
2017-01-06 13:35:40 -0500

    wrap root component with provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import {Provider} from 'react-redux';
+import {Router, browserHistory} from 'react-router';
+import configureStore from './configureStore';
+import routes from './routes';
 import './index.css';
 
+const store = configureStore();
+
 ReactDOM.render(
-  <App />,
+  (
+    <Provider store={store}>
+      <Router history={browserHistory} routes={routes}/>
+    </Provider>
+  ),
   document.getElementById('root') as HTMLElement
 );
