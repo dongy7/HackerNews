5ffc6089da9bf3f3e25f729edc81f62a5bef098e
dongy7 <dongy7@gmail.com>
2017-01-07 14:54:32 -0500

    add sidebar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,61 @@
 import * as React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import {Link} from 'react-router';
 
-class Navbar extends React.Component<null, null> {
+interface Props {
+  open: boolean;
+  onChange: Function;
+  onClose: Function;
+  onToggle: Function;
+}
+
+interface NavMenuItem {
+  key: number;
+  route: string;
+  text: string;
+}
+
+const menuItems: NavMenuItem[] = [
+  {
+    key: 0,
+    route: '/feed/topstories',
+    text: 'Top Stories',
+  },
+  {
+    key: 1,
+    route: '/feed/newstories',
+    text: 'New Stories',
+  },
+];
+
+class Navbar extends React.Component<Props, null> {
   render() {
     return (
-      <AppBar
-        title="Hacker News"
-      />
+      <div>
+        <AppBar
+          title="Hacker News"
+        />
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.props.open}
+          onRequestChange={() => this.props.onChange()}
+        >
+          {menuItems.map(menu => {
+            return (
+              <MenuItem
+                key={menu.key}
+                onTouchTap={() => this.props.onClose()}
+                containerElement={<Link to={menu.route} />}
+              >
+                {menu.text}
+              </MenuItem>
+            )
+          })}
+        </Drawer>
+      </div>
     );
   }
 };
