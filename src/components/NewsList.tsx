6ef87b337fa2682eb09eaa21dafb20934ecccfe2
dongy7 <dongy7@gmail.com>
2017-01-08 14:25:26 -0500

    add grid layout for news list

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -1,28 +1,40 @@
 import * as React from 'react';
+import {Card} from 'material-ui/Card';
 import {List, ListItem} from 'material-ui/List';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 const NewsList = (props: { news: Story[], onClick: Function }) => {
   return (
-    <List>
-      {props.news.map(story => {
-        const infoText = (
-          <div>
-            {`${ story.points } points by ${ story.user } ${story.time_ago} | `}
-            <a onClick={() => props.onClick(story.id)}>
-              {`${ story.comments_count } comments`}
-            </a>
-          </div>
-        );
-        return (
-          <ListItem
-            key={story.id}
-            primaryText={<a href={story.url}>{story.title}</a>}
-            secondaryText={infoText}
-            disabled
-          />
-        );
-      })}
-    </List>
+    <Grid>
+      <Row>
+        <Col xs={2} md={3} />
+        <Col xs={8} md={12}>
+          <Card>
+            <List>
+              {props.news.map(story => {
+                const infoText = (
+                  <div>
+                    {`${ story.points } points by ${ story.user } ${story.time_ago} | `}
+                    <a onClick={() => props.onClick(story.id)}>
+                      {`${ story.comments_count } comments`}
+                    </a>
+                  </div>
+                );
+                return (
+                  <ListItem
+                    key={story.id}
+                    primaryText={<a href={story.url}>{story.title}</a>}
+                    secondaryText={infoText}
+                    disabled
+                  />
+                );
+              })}
+            </List>
+          </Card>
+        </Col>
+        <Col xsHidden md={3} />
+      </Row>
+    </Grid>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import configureStore from './configureStore';
 import routes from './routes';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 const store = configureStore();
