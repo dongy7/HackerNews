5446889d6e2b4ec1e8442744e03e07ad050d2d06
dongy7 <dongy7@gmail.com>
2017-01-09 17:40:38 -0500

    add nav reducer tests

--- src/reducers/nav.spec.ts
+++ src/reducers/nav.spec.ts
@@ -1,9 +1,52 @@
-import navReducer from '../../src/reducers/nav';
+import navReducer from './nav';
+import * as actions from '../actions/nav';
 
 describe('navigation reducer', () => {
   it('should return the initial state', () => {
     expect(navReducer(undefined, {
       type: 'FOO', payload: 'foo', metadata: undefined
     })).toEqual(false);
+  });
+
+  it('should toggle nav state', () => {
+    expect(navReducer(true, {
+      type: actions.TOGGLE_NAV,
+    })).toBe(false);
+
+    expect(navReducer(false, {
+      type: actions.TOGGLE_NAV,
+    })).toBe(true);
+  });
+
+  it('should change nav status', () => {
+    expect(navReducer(true, {
+      type: actions.CHANGE_NAV,
+      payload: false,
+    })).toBe(false);
+
+    expect(navReducer(false, {
+      type: actions.CHANGE_NAV,
+      payload: false,
+    })).toBe(false);
+
+    expect(navReducer(true, {
+      type: actions.CHANGE_NAV,
+      payload: true,
+    })).toBe(true);
+
+    expect(navReducer(false, {
+      type: actions.CHANGE_NAV,
+      payload: true,
+    })).toBe(true);
+  });
+
+  it('should close the nav', () => {
+    expect(navReducer(true, {
+      type: actions.CLOSE_NAV,
+    })).toBe(false);
+
+    expect(navReducer(false, {
+      type: actions.CLOSE_NAV,
+    })).toBe(false);
   });
 });
